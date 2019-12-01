650ae8abca39a50f04c91ca60fad276f0a5e6e96
phankerson4 <phankerson4@gmail.com>
2019-12-01 05:55:47 +0000

    Divisible By Program

---
+++ divisible.rb
@@ -0,0 +1,46 @@
+def divisible_by_2(x)
+array = []
+  if x%2 == 0
+    array.push(x)
+    puts "#{x} is divisible by 2"
+  else
+  puts "#{x} is not divisible by 2"
+  
+  end
+
+
+end
+
+divisible_by_2(10)
+
+
+def divisible_by_3(x)
+array = []
+  if x%3 == 0
+    array.push(x)
+    puts "#{x} is divisible by 3"
+  else
+  puts "#{x} is not divisible by 3"
+  
+  end
+
+
+end
+
+divisible_by_3(15)
+
+
+def divisible_by_5(x)
+array = []
+  if x%5 == 0
+    array.push(x)
+    puts "#{x} is divisible by 5"
+  else
+  puts "#{x} is not divisible by 5"
+  
+  end
+
+
+end
+
+divisible_by_5(25)
